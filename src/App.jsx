94a8242a22a25fb2cc8f1c66ecb924fccac2ff32
Louis-Miguel <lpfermiza@up.edu.ph>
2024-04-20 21:51:17 +0800

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex flex-row justify-center">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-7xl">First Commit Check</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p className="mt-7">Just to check if GitHub Pages work on Vite React</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
