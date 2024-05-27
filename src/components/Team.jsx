465483bc6fad6b33a9b0ec52364ab1d436cea8db
Louis-Miguel <lpfermiza@up.edu.ph>
2024-05-27 21:34:57 +0800

    updated the website

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,4 +1,3 @@
-
 import Section from "./Section";
 import TeamCard from "./TeamCard";
 
@@ -8,23 +7,25 @@ const Team = () => {
       <h1 class="mb-6 text-center text-xl font-bold md:text-3xl">
         Meet our Team!
       </h1>
-     <div class="mx-auto my-16 flex max-w-7xl flex-wrap justify-center gap-8">
-      
+      <div class="mx-auto my-16 flex max-w-7xl flex-wrap justify-center gap-8">
         <TeamCard
           title={"Louis Miguel Fermiza"}
-          picture={"louis.jpg"}
-          description={"A Computer Science Student with a passion for challenging himself with new things in life."}
+          picture={"cs-132-project-portfolio/public/louis.jpg"}
+          description={
+            "A Computer Science Student with a passion for challenging himself with new things in life."
+          }
         />
         <TeamCard
           title={"Alfonso Luis De Los Reyes"}
-          picture={"luis.jpg"}
-          description={"Transferee Computer Science Student waiting to transfer my ideas from paper to reality"}
+          picture={"cs-132-project-portfolio/public/luis.jpg"}
+          description={
+            "Transferee Computer Science Student waiting to transfer my ideas from paper to reality"
+          }
         />
         <TeamCard
-          title={"Anthony Aaron Lazaro"} 
-          picture={"anthony.jpg"}
+          title={"Anthony Aaron Lazaro"}
+          picture={"cs-132-project-portfolio/public/anthony.jpg"}
         />
-
       </div>
     </Section>
   );
